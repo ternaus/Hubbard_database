dc886302e702fd2513f4594795c86b85ef1dbaa1
Vladimir Iglovikov <iglovikov@gmail.com>
2015-01-30 16:03:08 -0800

    Data for the square lattice added.

--- src/extra.py
+++ src/extra.py
@@ -1,1 +1,8 @@
-extra = {'name_of_the_lattice': "'square'"}
+extra = {'name_of_the_lattice': "'square'",
+         # 'north': 5,
+         # 'bcond': "'(0.0, 0.0, 0.0)'",
+         # 'tausk': 1,
+         # 'nbin': 5,
+         # 'errrate': 0.01,
+         # 'difflim': 0.01
+}
